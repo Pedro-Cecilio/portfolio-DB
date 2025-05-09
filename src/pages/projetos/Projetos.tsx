f39c14819b4945c55a265c79dc04e042c0b4aa33
Pedro-Cecilio <cardev604@gmail.com>
2025-05-09 16:21:34 -0300

    feat: acionando evento pageview-projetos gtm

--- src/pages/projetos/Projetos.tsx
+++ src/pages/projetos/Projetos.tsx
@@ -1,9 +1,16 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import ListaDeProjetos from "../../components/listaDeProjetos/ListaDeProjetos";
+import { useEffect } from "react";
 
 
 const Projetos = () => {
-
+    useEffect(() => {
+        window.dataLayer = window.dataLayer ?? [];
+        window.dataLayer.push({
+            event: 'pageview-projetos'
+        });
+    }, []);
+    
     return (
         <Flex flexDirection={"column"} mx={{base: 8, sm:20}} >
             <Flex flexDirection={"column"}>
